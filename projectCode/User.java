9aacdd24293d8a2ac087d7c564deaa36b73af67b
JustinRigby <justindrigby@gmail.com>
2023-10-12 14:04:42 -0400

    fixed #3

---
+++ projectCode/User.java
@@ -0,0 +1,29 @@
+package projectCode;
+import java.util.ArrayList;
+public class User {
+    protected String firstName;
+    protected String lastName; 
+    protected String userName;
+    protected int UUID;
+    protected String password;
+    public ArrayList<Project> projects;
+
+
+    public void User(String firstName, String lastName, String userName, String password, int UUID){
+         
+    }
+
+    public Project joinProject(Project project){
+        return project;
+    }
+    public void leaveProject(Project project){
+
+    }
+
+    public void removePermission(User user, UserType type){
+
+    }
+    public void addPermission(User user, UserType type){
+        
+    }
+}
