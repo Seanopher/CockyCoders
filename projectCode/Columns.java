1a16915d5c338c7489c2fce55de34641aed8cad5
JustinRigby <justindrigby@gmail.com>
2023-10-12 14:41:46 -0400

    #8

---
+++ projectCode/Columns.java
@@ -0,0 +1,14 @@
+package projectCode;
+import java.util.ArrayList;
+public class Columns {
+    public Arraylist<Task> column;
+    public String title;
+    public ArrayList<String> titles;
+
+    public Columns createColumn(Columns column, String title){
+        return column;
+    }
+    public Columns removeColumn(Columns column, String title){
+        return column;
+    }
+}
