090bb72f3cd0a1bab7d335dc855cc6f82bb3cf43
JustinRigby <justindrigby@gmail.com>
2023-10-15 22:51:34 -0400

    fixed #6

---
+++ projectCode/TaskList.java
@@ -0,0 +1,17 @@
+package projectCode;
+import java.util.ArrayList;
+public class TaskList {
+    private TaskList taskList;
+    private ArrayList<Task> tasks;
+
+    private TaskList(){
+
+    }
+    public Task getInstance(){
+        return getInstance();
+    }
+
+    public ArrayList<Task> getTasks(String taskName){
+        return this.tasks;
+    }
+}
