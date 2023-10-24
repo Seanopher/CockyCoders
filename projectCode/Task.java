7e6934e6030cbe10837ffc927bec2537c1d68bb8
Hunter <hunterhughes2787@gmail.com>
2023-10-24 13:26:41 -0400

    #24 worked on the sve and comment methods. Changed their types to void

--- projectCode/Task.java
+++ projectCode/Task.java
@@ -1,6 +1,12 @@
 package projectCode;
 import java.util.ArrayList;
 import java.util.UUID;
+//added more imports statements
+import java.io.IOException;
+import java.io.FileWriter;
+import org.json.simple.JSONObject;
+
+
 public class Task {
     protected UUID taskID;
     public String taskName;
@@ -17,20 +23,33 @@ public class Task {
         return newTask(taskName, description, assignedUser, document, taskType);
     }
 
+    /* 
+    //Taking out this method
     public void addPoint(User user){
 
     }
-
+    */
+    
     public Columns changeColumn(Columns columnFrom, Columns columnsTo){
         return changeColumn(columnFrom, columnsTo);
     }
 
-    public String comment(String comment){
-        return comment;
+    //changed to a void (was String) to only change 
+    public void comment(String comment){
+        Comment newComment = new Comment();
+        comments.add(newComment);
     }
 
-    public Task save(Task task){
-        return save(task);
+    // changed to void from "Task". 
+    public void save(Task task){
+        JSONObject taskJSON = DataWriter.getTaskJSON(this);
+
+        try (FileWriter file = new FileWriter(DataConstants.TASK_FILE_NAME)) {
+            file.write(taskJSON.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Task accessHistory(Task task, User user){
