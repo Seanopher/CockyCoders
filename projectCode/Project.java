15f24cfb439a2dda0654e211c03e8de303fb64b7
JustinRigby <justindrigby@gmail.com>
2023-10-26 14:08:18 -0400

    fixing issue

--- projectCode/Project.java
+++ projectCode/Project.java
@@ -1,30 +1,36 @@
 package projectCode;
 import java.util.UUID;
 import java.util.ArrayList;
+import java.util.HashMap;
 public class Project 
 {
     public String name;
+    public String pID;
     protected UUID projectID;
-    protected ArrayList<User> users;
+    protected UserList users;
+    protected ArrayList<Columns> column;
+    protected HashMap<String, TaskList> columns;
+    private static Project project;
     
 
 
-    public Project(String name, UUID projectID, ArrayList<User> users)
+    public Project(String name, UUID projectID, UserList users)
     {
         if(name != null && projectID != null && users != null)
         {
             this.name = name;
-            this.projectID = projectID;
-            this.users = users;
+            this.pID = projectID.toString();
+            this.users = UserList.getInstance();
+        }
+    }
+    public boolean saveProject(Project project){
+        return DataWriter.saveProject();
+    }
+    public static getInstance(String name, String projectID, UserList users){
+        if(project == null){
+            project = new Project();
         }
     }
     
-    public Columns createColumn(Columns column, String title){
-        return column;
-    }
-
-    public Columns removeColumn(Columns column, String title){
-        return column;
-    }
     
 }   
