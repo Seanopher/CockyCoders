04aee0ed35a12b7780a6f8d90927bb28b09d594b
EmmaLee Peyton <epeyton@email.sc.edu>
2023-10-26 12:47:27 -0400

    add code to Columns methods

--- projectCode/Columns.java
+++ projectCode/Columns.java
@@ -1,14 +1,57 @@
 package projectCode;
 import java.util.ArrayList;
-public class Columns {
-    public Arraylist<Task> column;
-    public String title;
-    public ArrayList<String> titles;
+import java.util.HashMap;
 
-    public Columns createColumn(Columns column, String title){
-        return column;
+/**
+ * @author EmmaLee Peyton
+ * this class encases columns using a hashmap
+ * it allows for the creation, removal, and the ability to move columns
+ */
+public class Columns {
+
+    private HashMap<String, ArrayList<TaskList>> column = new HashMap<String, ArrayList<TaskList>>();
+
+    /**
+     * 
+     * @param columns
+     * @param title
+     * @return
+     * 
+     */
+    public Columns createColumn(Columns columns, String title){
+        if (columns.column.containsKey(title)){
+            int i = 1;
+            while (columns.column.containsKey(title + "(" + i + ")"))
+                i++;
+        }
+        ArrayList<TaskList> taskList = new ArrayList<>();
+        columns.column.put(title, taskList);
+        return columns;
     }
-    public Columns removeColumn(Columns column, String title){
-        return column;
+
+    /**
+     * 
+     * @param columns
+     * @param title
+     * @return
+     */
+    public Columns removeColumn(Columns columns, String title){
+        columns.column.remove(title);
+        return columns;
     }
+
+    /**
+     * 
+     * @param columns
+     * @param title
+     * @return
+     */
+    public Columns moveColumn(Columns columns, String title){
+        String oldTitle = title;
+        ArrayList<TaskList> taskList = columns.column.get(oldTitle);
+        columns.column.remove(oldTitle);
+        columns.column.put(title, taskList);
+        return columns;
+    }
+    
 }
