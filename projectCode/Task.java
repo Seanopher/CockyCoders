d29bc7e081e24cd622710283c4ea8ed76cf2572d
Hunter <hunterhughes2787@gmail.com>
2023-11-01 17:57:41 -0400

    add comments to task and comment classes

--- projectCode/Task.java
+++ projectCode/Task.java
@@ -1,14 +1,11 @@
 package projectCode;
 import java.util.ArrayList;
-//add the built in date function
 import java.util.Date;
 import java.util.UUID;
-//added more imports statements
-import java.io.IOException;
-import java.io.FileWriter;
-import org.json.simple.JSONObject;
-import java.util.List;
-
+/**
+ * @author Hughes
+ * Creates the object task and assigns user varibles to the task
+ */
 public class Task {
     protected UUID taskID;
     public String taskName;
@@ -20,7 +17,15 @@ public class Task {
     public ArrayList<Comment> comments;
     public TaskType taskType;
     private ArrayList<Task> history;
-
+    /**
+     * Creates a new task
+     * @param taskName The name of the task
+     * @param description The descriotion that will be held by task
+     * @param assignedUser The user that is being assigned the task
+     * @param document The document being used by task
+     * @param taskType The type of the task, so it can be idenified by the user
+     * @return a new task
+     */
     public Task newTask(String taskName, String description, User assignedUser, String document, TaskType taskType){
         if ( taskName != null && description != null && assignedUser != null && document != null && taskType != null){
             this.taskName = taskName;
@@ -46,8 +51,6 @@ public class Task {
     public Date getDate(){
         return date;
     }
-
-    //is not included ??? 
     //Adding here to satisfy DataWriter
     public String getDueDate(){
         return null;
@@ -61,8 +64,10 @@ public class Task {
     public ArrayList<Comment> getComments(){
         return comments;
     }
-    
-    //changed to a void (was String) to only change 
+    /** 
+    * changed to a void (was String) to only change
+    * @param Comment uses comment to call the class and method
+    */
     public void comment(Comment comment){
         comments.add(comment);
     }
